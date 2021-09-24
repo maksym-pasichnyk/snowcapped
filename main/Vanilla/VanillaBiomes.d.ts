b8458538063c7445add17d288618fce29b10fde0
Jochen Jacobs <jochen.jacobs@campus.tu-berlin.de>
2021-09-24 15:39:51 +0200

    Updates

--- main/Vanilla/VanillaBiomes.d.ts
+++ main/Vanilla/VanillaBiomes.d.ts
@@ -1,6 +1,20 @@
 import { BiomeBuilder } from "../BuilderData/BiomeBuilder";
 export declare class VanillaBiomes {
-    private static biomes;
+    static biomes: ({
+        key: string;
+        name: string;
+        color: string;
+        r?: undefined;
+        g?: undefined;
+        b?: undefined;
+    } | {
+        name: string;
+        r: number;
+        g: number;
+        b: number;
+        key?: undefined;
+        color?: undefined;
+    })[];
     static registerVanillaBiomes(builder: BiomeBuilder): void;
     private static colorToHex;
 }
