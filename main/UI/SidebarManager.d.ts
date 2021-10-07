3dede121cf0df51454a1822bd045a0144762f713
Jochen Jacobs <JochenJacobs33@gmail.com>
2021-10-08 00:33:34 +0200

    Updates

--- main/UI/SidebarManager.d.ts
+++ main/UI/SidebarManager.d.ts
@@ -1,17 +1,27 @@
+import * as d3 from "d3";
 import { BiomeBuilder } from "../BuilderData/BiomeBuilder";
+import { LayoutElement } from "../BuilderData/LayoutElement";
+import { Slice } from "../BuilderData/Slice";
 export declare class SidebarManager {
-    private sidebar;
     private builder;
-    private layout_divs;
-    private biome_divs;
-    private bottom_spacer;
-    private search_bar;
     private dragType;
     private dragKey;
     private lastDragedOverDiv;
+    private openedElement;
+    private selectedElement;
     constructor(builder: BiomeBuilder);
+    private resizeBottomSpacer;
+    openElement(openElement?: {
+        type: string;
+        key?: string;
+    }): void;
+    selectElement(selectElement?: {
+        type: string;
+        key: string;
+    }): void;
     refresh(): void;
-    private updateBiomeSearch;
-    private createElementDiv;
+    handleElementDivs(list: (Slice | LayoutElement)[], c: string, selection: d3.Selection<d3.BaseType, unknown, HTMLElement, unknown>, fixed: boolean, use_color_picker: boolean): void;
+    dragover<T extends Slice | LayoutElement>(element: HTMLElement, c: string, list: T[], d: T): boolean;
+    drop<T extends Slice | LayoutElement>(element: HTMLElement, c: string, list: T[], d: T): boolean;
 }
 //# sourceMappingURL=SidebarManager.d.ts.map
